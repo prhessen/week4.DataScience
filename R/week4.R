54ccba9f4d35ddc2a5bc8f7bf62968798e6e86dd
Phoebe Hessen <hesse179@umn.edu>
2020-02-11 21:39:21 -0600

    Complete through line 10

--- R/week4.R
+++ R/week4.R
@@ -3,4 +3,8 @@ library(rstudioapi)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import
-
+library(tidyverse)
+week4_df <- read_delim(file = "../data/week4.dat", quote = "\"", delim = "-", col_names = c("casenum","parnum","stimver","datadate","qs"))
+glimpse(week4_df)
+week4_df <- separate(week4_df, qs, c("q1","q2","q3","q4","q5"))
+week4_df[5:9] <- sapply(X = week4_df[5:9], FUN = as.numeric)
